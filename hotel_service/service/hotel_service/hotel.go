c2f4924b1d05c3b5de6a6c8ed78974b6a5c23c05
Abduazim <yusupovabduazim0811@gmail.com>
2024-08-25 21:47:47 +0500

    hotel_service

--- hotel_service/service/hotel_service/hotel.go
+++ hotel_service/service/hotel_service/hotel.go
@@ -99,3 +99,80 @@ func (s *Service) DeleteHotel(ctx context.Context,req *hotelproto.HotelResponse)
 
 	return &hotelproto.HotelRes{Message: "Hotel Deleted"}, nil
 }
+
+func (s *Service) CreateRoom(ctx context.Context, req *hotelproto.RoomRequest)(*hotelproto.RoomResponse, error){
+	var roomreq hotel.RoomRequest
+	roomreq.Availability = req.Availability
+	roomreq.PricePerNight = req.PricePerNight
+	roomreq.RoomType = req.RoomType
+
+	res, err := s.service.Createroom(roomreq)
+	if err != nil {
+		log.Println("room created error")
+		return nil, fmt.Errorf("room created error: %v", err)
+	}
+
+	return &hotelproto.RoomResponse{RoomId: res.RoomID}, nil
+}
+
+func (s *Service) GetbyIdRoom(ctx context.Context,req *hotelproto.RoomResponse) (*hotelproto.Room, error){
+	var roomreq hotel.RoomResponse
+	roomreq.RoomID = req.RoomId
+	res, err := s.service.Getbyidroom(roomreq)
+	if err != nil {
+		log.Println("room get by id error")
+		return nil, fmt.Errorf("room get by id error: %v", err)
+	}
+
+	return &hotelproto.Room{
+		RoomId: res.RoomID,
+		RoomType: res.RoomType,
+		Availability: res.Availability,
+		PricePerNight: res.PricePerNight,
+	}, nil
+}
+
+func (s *Service) GetAllRooms(ctx context.Context, _ *hotelproto.Empty) (*hotelproto.ListRooms, error){
+	res, err := s.service.Getallroom()
+	if err != nil {
+		log.Println("get all room error")
+		return nil, fmt.Errorf("get all room error: %v", err)
+	}
+	var protoroom []*hotelproto.Room
+	for _, hotel := range *res{
+		protoroom = append(protoroom, &hotelproto.Room{
+			RoomId: hotel.RoomID,
+			Availability: hotel.Availability,
+			RoomType: hotel.RoomType,
+			PricePerNight: hotel.PricePerNight,
+		})
+	}
+
+	return &hotelproto.ListRooms{Room: protoroom}, nil
+}
+
+func (s *Service) UpdateRoom(ctx context.Context,req *hotelproto.Room) (*hotelproto.RoomRes, error){
+	var roomreq hotel.Room
+	roomreq.RoomID = req.RoomId
+	roomreq.RoomType = req.RoomType
+	roomreq.Availability = req.Availability
+	roomreq.PricePerNight = req.PricePerNight
+
+	err := s.service.Updateroom(roomreq)
+	if err != nil {
+		log.Println("update room error")
+		return nil, fmt.Errorf("update room error: %v", err)
+	}
+
+	return &hotelproto.RoomRes{Message: "room updated"}, nil
+}
+
+func (s *Service) DeleteRoom(ctx context.Context, req *hotelproto.RoomResponse) (*hotelproto.RoomRes, error){
+	err := s.service.Deleteroom(req.RoomId)
+	if err != nil {
+		log.Println("delete room error")
+		return nil, fmt.Errorf("delete room error: %v", err)
+	}
+
+	return &hotelproto.RoomRes{Message: "deleted room"}, nil
+}
